27c2672f97c9fbf61fa5d3be19edcfef58a023e0
Grzegorz Miebs <grzegorz.miebs@o2.pl>
2017-03-30 10:44:48 +0200

    beam search added

--- kod/dna.c
+++ kod/dna.c
@@ -2,6 +2,10 @@
 #include<stdlib.h>
 #include<omp.h>
 #include<string.h>
+#include<unistd.h>
+
+#define K 3
+#define Z 2
 
 int **graph, l, n, s;
 char **words;
@@ -12,6 +16,79 @@ int calculateDistance(char* a, char* b){
           return i; 
     }
     return l;
+}
+
+void wiazkowy(){
+    int visited[K][n], path[K][s], length[K] = {1}, cost[K] = {l}, **best;
+    for (int i = 0; i < K; i++) {
+        length[i] = 1;
+        cost[i] = l;
+    }
+    best = (int**) malloc((K+1) * sizeof(int*));
+    for (int i = 0; i <= K; i++)
+        best[i] = (int*) malloc(3 * sizeof(int));
+#pragma omp parallel for
+    for (int i = 0; i < K; i++)
+        for (int j = 0; j < n; j++)
+            visited[i][j] = 0;
+#pragma omp parallel for
+    for (int i = 0; i < K; i++){
+        path[i][0] = rand()%n;
+    }
+    while (cost[0] < s) {
+#pragma omp parallel for
+        for (int i = 0; i < K; i++) {
+            best[i][2] = 999999999;
+            best[i][1] = -1;
+            best[i][0] = -1;
+        }
+        for (int i = 0; i < K; i++) {
+            if (cost[i] < s){
+                for (int j = 0; j < n; j++) {
+                    best[K][0] = i;
+                    best[K][2] = cost[i] + graph[path[i][length[i]-1]][j];
+                    if (visited[i][j])
+                        best[K][2] += Z;
+                    best[K][1] = j;
+                    for (int k = K; k > 0; k--) {
+                        if (best[k][2] < best[k-1][2]) {
+                            int *tmp = best[k-1];
+                            best[k-1] = best[k];
+                            best[k] = tmp;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        int newLength[K], newPath[K][s];
+#pragma omp parallel for
+        for (int i = 0; i < K; i++) {
+            for (int j = 0; j < n; j++)
+                visited[i][j] = 0;
+        }
+        for (int i = 0; i < K; i++) {
+            printf("%d: ", i);
+            if (best[i][0] != -1) {
+                newLength[i] = length[best[i][0]]+1;
+                cost[i] = best[i][2];
+                for (int j = 0; j < length[i]; j++) {
+                    newPath[i][j] = path[best[i][0]][j];
+                    printf("%d ", newPath[i][j]);
+                    visited[i][newPath[i][j]] = 1;
+                }
+                newPath[i][newLength[i]-1] = best[i][1];
+                printf("%d\n", best[i][1]);
+            }
+        }
+#pragma omp parallel
+        for (int i = 0; i < K; i++){
+            length[i] = newLength[i];
+            for (int j = 0; j < length[i]; j++)
+                path[i][j] = newPath[i][j];
+        }
+    }
 }
 
 void readWords(char* file){
@@ -36,19 +113,25 @@ void readWords(char* file){
             if (i != j)
                 graph[i][j] = calculateDistance(words[i], words[j]);
             else 
-                graph[i][i] = 0;
+                graph[i][i] = 99999999;
         }
     }
     for (int i = 0; i < n; i++){
-        printf("\n");
         for (int j = 0; j < n; j++) {
             printf("%d ", graph[i][j]);
         }
+        printf("\n");
     }
-
+    //close(&fp);
 }
 
 int main() {
     readWords("test.txt");
-
+    wiazkowy();
+    for (int i = 0; i < n; i++){
+        free(graph[i]);
+        free(words[i]);
+    }
+    free(graph);
+    free(words);
 }
